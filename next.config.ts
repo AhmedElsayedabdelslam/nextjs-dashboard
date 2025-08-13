279ce4eea15ed48d48654795ebe59b17e3fe8e08
AhmedElsayedabdelslam <111446033+AhmedElsayedabdelslam@users.noreply.github.com>
2025-08-13 18:22:01 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // experimental: {
-  //   ppr: 'incremental'
-  // }
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
